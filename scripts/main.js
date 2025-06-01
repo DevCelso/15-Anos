306cc7aba4a230903ac68f9a3a0e9e9573f5927a
Celso Tadeu <celsotadeu75@gmail.com>
2025-05-31 23:44:27 -0300

    TesteFormulario

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
 const countdow = document.getElementById("countdown");
+const eventDate = new Date("2025-09-06T11:00:00");
+
+function updateCountdown() {
+    const now = new Date();
+    const diff = eventDate - now;
+
+    if (diff <= 0) {
+        countdow.textContent = "É hoje! ✨";
+        return
+    }
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
+    const minutes = Math.floor(diff / (1000 * 60)) % 60;
+    const seconds = Math.floor(diff / (1000)) % 60;
+
+    countdow.textContent = `${days} dias, ${hours}h ${minutes}m ${seconds}s`;
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
