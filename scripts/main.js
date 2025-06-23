254e0788212d312277dc1d30dc8805deeeb1c450
Celso Tadeu <celsotadeu75@gmail.com>
2025-06-22 23:06:09 -0300

    CarrosselFotos

--- scripts/main.js
+++ scripts/main.js
@@ -20,3 +20,15 @@ function updateCountdown() {
 
 setInterval(updateCountdown, 1000);
 updateCountdown();
+
+//Carrosel galeria
+let cont = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval(() => {
+    cont++;
+    if (cont > 3) cont = 1;
+    document.getElementById('radio' + cont).checked = true;
+}, 5000);
+
+
